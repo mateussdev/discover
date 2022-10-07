7fbf724b2905a90025388ffc4dd8e16494792582
mateussdev <mateusbs427@gmail.com>
2022-10-07 16:30:08 -0300

    consuming API with NodeJS

--- APIs/index.js
+++ APIs/index.js
@@ -1,12 +1,13 @@
 // CREATING A SERVER WITH EXPRESS
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 
-const app = express()
+const app = express();
 
-app.listen('3000')
+app.listen('3000');
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 /* API no Back-End
 // GET
@@ -54,6 +55,14 @@ app.route('/:identificador').delete( (req, res) => {
 // app.route('/identidade/:nome').get( (req, res) => res.send( req.params.nome ) )
 
 // QUERY PARAMS
-app.route('/').get( (req, res) => res.send( req.query ) )
+// app.route('/').get( (req, res) => res.send( req.query ) )
 
-app.route('/about/user').get( (req, res) => res.send( req.query.nome ) )
+// app.route('/about/user').get( (req, res) => res.send( req.query.nome ) )
+
+// API GitHub
+app.route('/').get( (req, res) => {
+  axios.get('https://api.github.com/users/mateussdev')
+  .then(result => res.send(result.data)) // pega todas as informações públicas de um perfil no github
+  // .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+  .catch(error => console.log(error))
+} );
