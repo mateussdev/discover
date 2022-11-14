5bdc543396bd524c039a0cbe4298e4ff0f4821a4
mateussdev <mateusbs427@gmail.com>
2022-11-14 16:52:59 -0300

    Estrutura de pastas, fragment, imoprtação de arquivos css, separando css, estilizando pagina css, importando fonte, componentes

---
+++ reactjs/reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export function Card() {
+  return (
+    <div className="card">
+      <strong>Mateus Silva</strong>
+      <small>10:00:08</small>
+    </div>
+  );
+}
---
+++ reactjs/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+import { Card } from "../../components/Card";
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+    </div>
+  );
+}
