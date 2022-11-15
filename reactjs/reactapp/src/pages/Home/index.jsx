2c30cc80022868ab4f482d4d423bb874d58fba09
mateussdev <mateusbs427@gmail.com>
2022-11-15 09:48:25 -0300

    Header

--- reactjs/reactapp/src/pages/Home/index.jsx
+++ reactjs/reactapp/src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ import { Card } from "../../components/Card";
 export function Home() {
   const [studentName, setStudentName] = useState("");
   const [students, setStudents] = useState([]);
-  
+
   function handleAddStudent() {
     const newStudent = {
       name: studentName,
@@ -23,20 +23,24 @@ export function Home() {
 
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>Mateus</strong>
+          <img src="https://github.com/mateussdev.png" alt="Foto de Perfil" />
+        </div>
+      </header>
       <input
         type="text"
         placeholder="Digite o nome..."
         onChange={e => setStudentName(e.target.value)}
       />
-      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
       {students.map(student => (
-        <Card 
-          key={student.time}
-          name={student.name} 
-          time={student.time} 
-        />
+        <Card key={student.time} name={student.name} time={student.time} />
       ))}
     </div>
   );
